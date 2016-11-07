e0b4a633fa7a78be20821cd8b1d3425d72b908c2
Ulysses <ulyssesfm@gmail.com>
2016-11-07 10:05:56 -0800

    changes

---
+++ scripts/gen.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#UlysiteGen
+
+use strict;
+use warnings;
+
+print "Ulysite generator";
+open my $fh, '<', "templates/index.html" or die "index file doesn't exist.";
+my $index = do {local $/; <$fh>};
+
+open $fh, '<', "templates/project.html" or die "project template doesn't exist.";
+my $project = do {local $/; <$fh>};
+
+open $fh, '<', "templates/essay.html" or die "essay template doesn't exist.";
